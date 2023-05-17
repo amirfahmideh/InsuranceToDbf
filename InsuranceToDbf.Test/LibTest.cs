0c27516b9e3afd73253a347cc9edd7fd67f9f11c
Amir Fahmideh <amir.fahmideh@gmail.com>
2023-05-17 12:24:24 +0330

    init test project

---
+++ InsuranceToDbf.Test/LibTest.cs
@@ -0,0 +1,16 @@
+using InsuranceToDbf.Convertor;
+using InsuranceToDbf.Lib;
+namespace InsuranceToDbf.Test;
+public class LibTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void DateTimeTest()
+    {
+        Assert.Pass();
+    }
+}
